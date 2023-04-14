53b056e79055fe7acad2b56aedb90fc86edbc763
Milind Sharma <milindsharma8@gmail.com>
2023-04-14 16:18:06 +0800

    Add inverter controller state machine

--- Inverter-Controller/src/main.cpp
+++ Inverter-Controller/src/main.cpp
@@ -1,33 +1,54 @@
-#include <eFlexPwm.h>
+#include <eFlexPwm.h>    // Version: 0.2.5
 #include <Arduino.h>
 
-#define DISABLE_PIN 24
-#define READ_STATE_PIN 25
+// TODO: Add PID loop for active mode.
 
+// Pin definitions
+#define DISABLE_PIN 24   // Used to disable/enable gate driver IC's outputs. low = enable, high = disable.
+#define READ_MODE_PIN_0 25    // Used to read the 0 bit of the mode
+#define READ_MODE_PIN_1 26    // Used to read the 1 bit of the mode
 
-uint8_t phaseShiftPercent = 50; // the phase shift in %
+#define STANDBY_PHASE_SHIFT 20    // Small phase shift value for the standby mode.
+#define OFFLINE_PHASE_SHIFT 50    // 50% or 90 degrees for offline standard mode.
+
+enum mode{
+  OFF = 0,
+  STANDBY = 1,
+  ACTIVE = 2,
+  OFFLINE = 3,
+};
+
+uint8_t phaseShiftPercent = 0; // the phase shift in % .  // ? Is this even required?
+uint32_t delayMicros = 0; // delay time in microseconds.
+uint8_t mode = mode::OFF; 
+uint8_t prevMode = mode::OFF;
 const uint32_t PwmFreq = 100000; // frequency of PWM = 100kHz
 
-enum stateOptions {OFF, ON};
-uint8_t state = ON;
-uint8_t prevState = ON;
-
 using namespace eFlex;
-// My eFlexPWM submodules (Hardware > PWM2: SM[0], SM[2]
+// eFlexPWM submodules (Hardware -> PWM2: SM[0], SM[2]
 SubModule Sm20(4, 33);
 SubModule Sm22(6, 9);
 //  Tm2 simplifies access to the functions that concern all the sub-modules
 Timer &Tm2 = Sm20.timer();
 
+// Read the mode pins and return the mode.
+uint8_t checkMode() {
+  uint8_t mode = 0;
+  mode |= digitalRead(READ_MODE_PIN_0);
+  mode |= digitalRead(READ_MODE_PIN_1) << 1;
+  return mode;
+}
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(DISABLE_PIN, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(READ_STATE_PIN, INPUT_PULLUP);    // Default HIGH, ie, default state is ON
+  pinMode(LED_BUILTIN, OUTPUT);    // Temporarily used as a mode indicator. off = OFF, blink = STANDBY, on = ACTIVE, strobe = OFFLINE.
+  // For the read mode pins, default is LOW. Should result in OFF mode in case of no input.
+  pinMode(READ_MODE_PIN_0, INPUT_PULLDOWN);
+  pinMode(READ_MODE_PIN_1, INPUT_PULLDOWN);
 
-  digitalWrite(DISABLE_PIN, LOW);    // Low = enabled
+  digitalWrite(DISABLE_PIN, HIGH);    // high = disable, low = enable
 
   Config myConfig;
   myConfig.setReloadLogic(kPWM_ReloadPwmFullCycle);
@@ -48,34 +69,60 @@ void setup() {
   // synchronize registers and start all submodules
   Tm2.begin();
 
-  Sm22.setupPwmPhaseShift(ChanA, phaseShiftPercent, true);
+  Sm22.setupPwmPhaseShift(ChanA, phaseShiftPercent, true); 
 }
  
 void loop() {
-  // Read the on/off state. If it has changed, update the inverter state.
-  state = digitalRead(READ_STATE_PIN);
-  if (state != prevState) {
-    if (state == ON) {
-      digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(DISABLE_PIN, LOW);
-    } else if (state == OFF) {
-      digitalWrite(LED_BUILTIN, LOW);
-      digitalWrite(DISABLE_PIN, HIGH);
-    } else {
-      Serial.println("Error: state not recognized");
-      Serial.println("Inverter switched off for safety");
-      digitalWrite(LED_BUILTIN, LOW);
-      digitalWrite(DISABLE_PIN, HIGH);
+  // Check mode
+  mode = checkMode();
+
+  // Check if mode changed from previous mode
+  if (mode != prevMode) {
+    // Mode changed, update previous mode
+    // Set the mode.
+    switch (mode) {
+      case mode::OFF:
+        // Switch off inverter.
+        Serial.printf("{%s, %d}: mode: OFF\n", __FILE__, __LINE__);
+        digitalWrite(DISABLE_PIN, HIGH);  // disables MOSFET driver ICs output.
+        digitalWrite(LED_BUILTIN, LOW);   // Visual indication of mode. LED if off.
+        delayMicros = 0;
+        break;
+
+      case mode::STANDBY:
+        Serial.printf("{%s, %d}: mode: STANDBY\n", __FILE__, __LINE__);
+        // Switch on inverter in standby mode. This increases phase shift, pulses the output every 500ms, reduces power consumption.
+        digitalWrite(DISABLE_PIN, !digitalRead(DISABLE_PIN));    // Toggles inverter output.
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));   // Visual indication of mode. LED blinks.
+        Sm22.setupPwmPhaseShift(ChanA, STANDBY_PHASE_SHIFT, true);
+        delayMicros = 500000;    // 500ms
+        break;
+
+      case mode::ACTIVE:
+        Serial.printf("{%s, %d}: mode: ACTIVE\n", __FILE__, __LINE__);
+        digitalWrite(DISABLE_PIN, LOW);
+        digitalWrite(LED_BUILTIN, HIGH);
+        // run PID loop with secondary voltage until achieved
+        // TODO MAKE THIS work
+        // delay 100 PWM periods
+        delayMicros = (1000000U / PwmFreq) * 100;
+        break;
+
+      case mode::OFFLINE:
+        Serial.printf("{%s, %d}: mode: OFFLINE\n", __FILE__, __LINE__);
+        digitalWrite(DISABLE_PIN, LOW);
+        digitalWrite(LED_BUILTIN, LOW);
+        Sm22.setupPwmPhaseShift(ChanA, OFFLINE_PHASE_SHIFT, true);
+        break;
+
+      default:
+        digitalWrite(DISABLE_PIN, HIGH);
+        digitalWrite(LED_BUILTIN, LOW);
+        delayMicros = 0;
+        break;
     }
+    prevMode = mode;
   }
   
-  // // make the PWM phase shift oscillate between 0 and 50%
-  // phaseShiftPercent = (phaseShiftPercent + 1) % 50;
-
-  // // Set the PWM phase shift to the new value
-  // Sm22.setupPwmPhaseShift(ChanA, phaseShiftPercent, true);
-
-  // delayMicroseconds ( (1000000U / PwmFreq) * 100);     // Delay at least 100 PWM periods
-  // delay(100);
-  prevState = state;
+  delayMicroseconds(delayMicros);   // loop delay for stability.
 }
